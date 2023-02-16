ff5ff893e3b092f847cb78f3318f7231c972c733
Lisx5JP <starunbian@foxmail.com>
2023-02-16 18:04:58 +0800

    fix svg

--- packages/talkee/src/components/icons/IconGift.tsx
+++ packages/talkee/src/components/icons/IconGift.tsx
@@ -1,9 +1,9 @@
 export function IconGift() {
   return (
     <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
+      width="12"
+      height="14"
+      viewBox="0 0 12 14"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
